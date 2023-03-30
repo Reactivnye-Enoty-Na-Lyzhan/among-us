529c856c9b489fb25098542a4e2eeede8dba831c
Artur Isaev <isaev.artur.1999@mail.ru>
2023-03-30 14:43:47 +0600

    wallpaper css: changed class selector

--- packages/client/src/components/LandingPage/Wallpaper/Wallpaper.tsx
+++ packages/client/src/components/LandingPage/Wallpaper/Wallpaper.tsx
@@ -3,14 +3,14 @@ import './Wallpaper.css';
 export default function () {
   return (
     <div className="background-wallpaper">
-      <div className="background-wallpaper__stars-1"></div>
-      <div className="background-wallpaper__stars-2"></div>
-      <div className="background-wallpaper__stars-3"></div>
-      <div className="background-wallpaper__stars-4"></div>
-      <div className="background-wallpaper__stars-5"></div>
-      <div className="background-wallpaper__stars-6"></div>
-      <div className="background-wallpaper__stars-7"></div>
-      <div className="background-wallpaper__stars-8"></div>
+      <div className="background-wallpaper__stars-1 animated-wallpaper"></div>
+      <div className="background-wallpaper__stars-2 animated-wallpaper"></div>
+      <div className="background-wallpaper__stars-3 animated-wallpaper"></div>
+      <div className="background-wallpaper__stars-4 animated-wallpaper"></div>
+      <div className="background-wallpaper__stars-5 animated-wallpaper"></div>
+      <div className="background-wallpaper__stars-6 animated-wallpaper"></div>
+      <div className="background-wallpaper__stars-7 animated-wallpaper"></div>
+      <div className="background-wallpaper__stars-8 animated-wallpaper"></div>
     </div>
   );
 }
