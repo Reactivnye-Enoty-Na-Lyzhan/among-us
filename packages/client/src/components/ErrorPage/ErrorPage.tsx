60b0b2094f9834908c5df3e2f70567f2c7237683
tzhovtyi <tzzzhovtyy@gmail.com>
2023-04-01 02:58:49 +1000

    Add error pages

---
+++ packages/client/src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import "./ErrorPage.css";
+
+type Props = {
+    code: string,
+    message: string,
+    linkLabel: string
+}
+
+function ErrorPage({code, message, linkLabel}: Props) {
+    return (
+        <div className="error-page">
+            <h1 className="error-page__code">{code}</h1>
+            <h5 className="error-page__message">{message}</h5>
+            <Link to={'/'} className="error-page__return-link">
+                {linkLabel}
+            </Link>
+        </div>
+    );
+}
+
+export const Error404 = () => <ErrorPage 
+                            code="404" 
+                            message="Игрок, ты, кажется, вышел не в тот шлюз.
+                            Здесь пустота..." 
+                            linkLabel="Давай вернёмся назад?"
+                        />;
+export const Error500 = () => <ErrorPage 
+                            code="5**" 
+                            message="Центр управления потерял связь с кораблём.
+                            Никто не знает, что там может происходить прямо сейчас..." 
+                            linkLabel="Можем попробовать вернуться назад?"
+                        />;
