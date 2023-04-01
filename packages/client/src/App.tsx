91a6fd1f047340e3a86b8c3db613e24521ae49ae
Timofei Zhovtyi <87607555+tzhovtyi@users.noreply.github.com>
2023-04-02 01:29:55 +1000

    Обработка ошибок (#11)
    Co-authored-by: Alex Efremov <efremov.sakha@gmail.com>

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
-import LoginPage from './components/LoginPage/LoginPage';
 import './vendor/fonts/Inter-Regular.woff';
 import './vendor/fonts/Inter-Medium.woff';
 import './vendor/fonts/Inter-Bold.woff';
+import { Error404 } from './components/ErrorPage/ErrorPage';
 import './App.css';
+import LoginPage from './components/LoginPage/LoginPage';
+import './fonts/Inter-Regular.woff';
+import './fonts/Inter-Medium.woff';
+import './fonts/Inter-Bold.woff';
+import ErrorToast from './components/ErrorToast/ErrorToast';
 
 //placeholders
 function ForumPage() {
@@ -16,20 +21,18 @@ function GamePage() {
 function SignUpPage() {
   return <h1>Registration</h1>;
 }
-function NotFound() {
-  return <h1>404</h1>;
-}
 
 function App() {
   return (
     <Router>
+        <ErrorToast />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/game" element={<GamePage />} />
         <Route path="/forum" element={<ForumPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Error404/>} />
       </Routes>
     </Router>
   );
