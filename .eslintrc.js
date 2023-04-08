9436db12a2f436dde449a79de750487e4fb4bce0
Artur Isaev <isaev.artur.1999@mail.ru>
2023-04-08 21:32:59 +0600

    return configs without stylelint, eslint/import

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,47 +7,26 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
   },
-  plugins: ['@typescript-eslint', 'import', 'jsx-a11y', 'react', 'react-hooks'],
+  plugins: ['@typescript-eslint',
+            "import",
+            "jsx-a11y",
+            "react",
+            "react-hooks"
+        ],
   rules: {
     '@typescript-eslint/ban-ts-comment': 1,
     '@typescript-eslint/no-explicit-any': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
     'react/react-in-jsx-scope': 'off',
     'import/extensions': 'off',
-    'eol-last': ['error', 'always'],
-    'import/order': [
-      'warn',
-      {
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: false,
-        },
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-          'unknown',
-        ],
-      },
-    ],
-    semi: ['error', 'always'],
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        project: ['packages/client/tsconfig.json'],
-      },
-    },
+    "eol-last": ["error", "always"],
+    "semi": ['error', 'always'],
   },
   ignorePatterns: ['node_modules/'],
 };
