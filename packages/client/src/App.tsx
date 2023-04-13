13ec1f21545b2fe44a5d638f299f9d290ba85c8c
artiquanta <93656865+artiquanta@users.noreply.github.com>
2023-04-13 15:49:07 +0700

    feature/rea-37 (#14)

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -8,6 +8,7 @@ import './vendor/fonts/Inter-Medium.woff';
 import './vendor/fonts/Inter-Bold.woff';
 import { Error404 } from './components/ErrorPage/ErrorPage';
 import ErrorToast from './components/ErrorToast/ErrorToast';
+import LeaderBoard from './components/LeaderBoard/LeaderBoard';
 import LoginPage from './components/LoginPage/LoginPage';
 import SignUpPage from './components/SignUpPage/SignUpPage';
 import ForumPage from './components/Forum/Page/Page';
@@ -28,6 +29,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/game" element={<GamePage result="win" score={10} />} />
+        <Route path="/leaderboard" element={<LeaderBoard />} />
         <Route path="/forum" element={<ForumPage />} />
         <Route path='/profile' element={<Profile choice={link} handleChoiceChange={handleChoiceChange}/>} />
         <Route path="*" element={<Error404/>} />
