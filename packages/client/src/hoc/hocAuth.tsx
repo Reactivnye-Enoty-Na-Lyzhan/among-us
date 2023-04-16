f430dc6a455d3a36d5c0a434a8da89f3daaf653e
Artur Isaev <isaev.artur.1999@mail.ru>
2023-04-17 02:18:17 +0600

    add generic typization to hocAuth

--- packages/client/src/hoc/hocAuth.tsx
+++ packages/client/src/hoc/hocAuth.tsx
@@ -1,21 +1,23 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import useAuth from '../hooks/useAuth';
 import { Navigate } from 'react-router-dom';
 
-const hocAuth = (Component: FC) => {
-  return () => {
+function hocAuth<Props extends Record<string, unknown>>(
+  Component: FC<Props>
+): FC<Props> {
+  return props => {
     const { isAuthenticated, isLoading } = useAuth();
-    
+
     if (isLoading) {
       return <div>Loading...</div>;
     }
 
     if (isAuthenticated) {
-      return <Component />;
+      return <Component {...props} />;
     } else {
       return <Navigate to="/login" />;
     }
   };
-};
+}
 
 export default hocAuth;
