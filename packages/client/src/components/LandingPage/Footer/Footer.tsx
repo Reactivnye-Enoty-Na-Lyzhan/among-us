c3ddfeb63f9ebacce7621d08139a88a9d226b8c0
Artur Isaev <isaev.artur.1999@mail.ru>
2023-04-19 12:00:59 +0600

    change color scheme; add link to /game; translate to russian

--- packages/client/src/components/LandingPage/Footer/Footer.tsx
+++ packages/client/src/components/LandingPage/Footer/Footer.tsx
@@ -1,52 +1,56 @@
 import postcssLogo from 'images/landing-page/icons/postcss-logo.svg';
 import reactLogo from 'images/landing-page/icons/react-logo.svg';
+import reduxLogo from 'images/landing-page/icons/redux-logo.svg';
 import viteLogo from 'images/landing-page/icons/vite-logo.svg';
 import './Footer.css';
+
+function TechnologyEntry({
+  technologyWebsiteURL,
+  imageSource,
+  imageAlt,
+}: {
+  technologyWebsiteURL: string;
+  imageSource: string;
+  imageAlt: string;
+}) {
+  return (
+    <li className="technologies-list-entry">
+      <a
+        href={technologyWebsiteURL}
+        className="technologies-list-entry__link"
+        target="_blank">
+        <img
+          className="technologies-list-entry__logo"
+          src={imageSource}
+          alt={imageAlt}
+        />
+      </a>
+    </li>
+  );
+}
 
 export default function () {
   return (
     <footer className="footer">
       <section className="technologies-list-section">
-        <h3 className="technologies-list-section__header">
-          Technologies Used:
-        </h3>
+        <h3 className="technologies-list-section__header">Технологии:</h3>
         <ul className="technologies-list">
-          <li className="technologies-list-entry">
-            <a
-              href="https://vitejs.dev/"
-              className="technologies-list-entry__link"
-              target="_blank">
-              <img
-                className="technologies-list-entry__logo"
-                src={viteLogo}
-                alt="vite logo"
-              />
-            </a>
-          </li>
-          <li className="technologies-list-entry">
-            <a
-              href="https://react.dev/"
-              className="technologies-list-entry__link"
-              target="_blank">
-              <img
-                className="technologies-list-entry__logo"
-                src={reactLogo}
-                alt="react logo"
-              />
-            </a>
-          </li>
-          <li className="technologies-list-entry">
-            <a
-              href="https://postcss.org/"
-              className="technologies-list-entry__link"
-              target="_blank">
-              <img
-                className="technologies-list-entry__logo"
-                src={postcssLogo}
-                alt="postcss logo"
-              />
-            </a>
-          </li>
+          <TechnologyEntry
+            technologyWebsiteURL="https://vitejs.dev/"
+            imageSource={viteLogo}
+            imageAlt="vite logo"></TechnologyEntry>
+          <TechnologyEntry
+            technologyWebsiteURL="https://react.dev/"
+            imageSource={reactLogo}
+            imageAlt="react logo"></TechnologyEntry>
+          <TechnologyEntry
+            technologyWebsiteURL="https://redux.js.org/"
+            imageSource={reduxLogo}
+            imageAlt="redux logo"></TechnologyEntry>
+          <TechnologyEntry
+            technologyWebsiteURL="https://postcss.org/"
+            imageSource={postcssLogo}
+            imageAlt="postcss logo"></TechnologyEntry>
         </ul>
       </section>
       <section className="media-links-section">
