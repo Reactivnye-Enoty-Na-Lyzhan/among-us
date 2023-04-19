2a171efa31b5eea1fd3853a627e2e608337fd6b0
Artur Issayev <32362017+Issayev@users.noreply.github.com>
2023-04-19 15:36:50 +0300

    Доработка Лэндинга (#32)

--- packages/client/src/components/LandingPage/Main/game-description-text.ts
+++ packages/client/src/components/LandingPage/Main/game-description-text.ts
@@ -1,19 +1,17 @@
 export default `
-Welcome to 'Among Us', the ultimate web browser game that takes
-inspiration from the wildly popular computer original. In this game,
-you'll find yourself on a spaceship mission with a team of fellow
-astronauts, but little do you know, there are infiltrators among you
-trying to sabotage the mission. As you navigate through the spaceship
-completing tasks, keep your eyes peeled for anyone acting
-suspiciously. You never know who the infiltrators might be, so it's
-important to work together with your team to uncover their true
-identity. When an emergency meeting is called, you'll have to defend
-your innocence and cast your vote for who you believe is the
-infiltrator. But be careful, because the infiltrators are skilled at
-deception and will stop at nothing to throw you off their trail. With
-immersive graphics and fast-paced gameplay, 'Space Infiltrators' will
-keep you on the edge of your seat as you work to uncover the
-infiltrators and save the mission. Can you work together with your
-team to save the day, or will the infiltrators succeed in their
-mission?
+Добро пожаловать в "Among Us", потрясающую браузерную игру, которая черпает
+вдохновение из популярного компьютерного оригинала. В этой игре вы окажетесь на
+космическом корабле с командой коллег-астронавтов, но вы знаете, что среди вас
+есть лазутчики, пытающиеся саботировать миссию. Перемещаясь по космическому
+кораблю и выполняя задания по его обслуживанию, следите за теми, кто ведет себя
+подозрительно. Вы никогда не знаете, кем могут быть лазутчики, поэтому важно
+работать вместе со своей командой, чтобы раскрыть их личность. Когда будет
+созвано экстренное совещание, вам придется отстаивать свою позицию, убеждая
+сокомандников в своей невиновности и отдавая голос против того, кого вы считаете
+тайным вредителем. Но будьте осторожны, потому что противники искусны в обмане и
+не остановятся ни перед чем, чтобы сбить вас с толку. Благодаря захватывающей
+графике и быстро развивающемуся игровому процессу "Among Us" заставит вас
+напрячься, пока вы работаете над раскрытием лазутчиков и спасением миссии.
+Сможете ли вы работать вместе со своей командой, чтобы спасти положение, или
+саботёры преуспеют в своей миссии?
 `;
