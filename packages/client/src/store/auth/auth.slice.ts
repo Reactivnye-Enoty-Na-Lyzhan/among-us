542b951e451e405abc2d1a32ee5a77cc2b513b69
tzhovtyi <tzzzhovtyy@gmail.com>
2023-04-19 23:44:54 +1000

    rebase onto sprint_6

--- packages/client/src/store/auth/auth.slice.ts
+++ packages/client/src/store/auth/auth.slice.ts
@@ -35,7 +35,9 @@ export const authApi = createApi({
         method: 'POST',
         body: data,
         responseHandler: response => {
-          const isJson = response.headers.get('Content-Type')?.includes('application/json');
+          const isJson = response.headers
+            .get('Content-Type')
+            ?.includes('application/json');
           return isJson ? response.json() : response.text();
         },
       }),
