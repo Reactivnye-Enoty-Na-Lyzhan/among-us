9afbaf3b20bfabbe47c4dd38583084a6c3301c28
Artur Isaev <isaev.artur.1999@mail.ru>
2023-04-20 00:18:32 +0600

    wrap Login Page with hocAuth; prettier formatting

--- packages/client/src/components/LoginPage/hooks/useSignIn.ts
+++ packages/client/src/components/LoginPage/hooks/useSignIn.ts
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import { useSignInUserMutation } from '@/store/auth/auth.slice';
-=======
->>>>>>> c393cfc (Fix RTKQuery behaviour on success)
 import { useState } from 'react';
 import { useSignInUserMutation } from '@/store/auth/auth.slice';
 import {
   isRTKQueryFetchError,
   isRTKQuerySuccessfulResponse,
 } from '@/utils/api/response-types';
-<<<<<<< HEAD
 import { APIErrorResponse, SignInRequestDTO } from '@/store/auth/auth.types';
-=======
-import {
-  SignUpRequestErrorResponse,
-  SignInRequestDTO,
-} from '@/store/auth/auth.types';
->>>>>>> c393cfc (Fix RTKQuery behaviour on success)
 import { getErrorMessage } from '@/utils/api/signup/error-messages/get-error-message';
 
 export function useSignIn() {
@@ -40,11 +29,7 @@ export function useSignIn() {
         if (status === 401) {
           errorMessage = 'Неверный логин или пароль';
         } else {
-<<<<<<< HEAD
           const response = error.data as APIErrorResponse;
-=======
-          const response = error.data as SignUpRequestErrorResponse;
->>>>>>> c393cfc (Fix RTKQuery behaviour on success)
           errorMessage = getErrorMessage({ status, response });
         }
         setStatusMessageClass('login-page__status_red');
@@ -58,10 +43,6 @@ export function useSignIn() {
       return false;
     }
   }
-<<<<<<< HEAD
-
-=======
->>>>>>> c393cfc (Fix RTKQuery behaviour on success)
   return {
     requestStatus,
     statusMessageClass,
