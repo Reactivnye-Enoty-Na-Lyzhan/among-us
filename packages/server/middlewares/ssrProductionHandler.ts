325514655804fc5195ed506fe4940ff9dc276ef2
quantum <partnerprods@yandex.ru>
2023-04-26 20:03:33 +0700

    style: format prettier

--- packages/server/middlewares/ssrProductionHandler.ts
+++ packages/server/middlewares/ssrProductionHandler.ts
@@ -3,22 +3,27 @@ import fs from 'fs';
 import path from 'path';
 import { CLIENT_PACKAGE_PATH } from '../utils/constants';
 
-export const ssrProductionHandler = async (req: Request, res: Response, next: NextFunction) => {
+export const ssrProductionHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const url = req.originalUrl;
 
   try {
     const template = fs.readFileSync(
       path.join(CLIENT_PACKAGE_PATH, './dist/index.html'),
-      'utf-8',
+      'utf-8'
     );
 
-    const { render } = await import(path.join(CLIENT_PACKAGE_PATH, './dist-ssr/ssr-entry.cjs'));
+    const { render } = await import(
+      path.join(CLIENT_PACKAGE_PATH, './dist-ssr/ssr-entry.cjs')
+    );
     const { renderedHtml } = await render(url);
 
     const html = template.replace(`<!--ssr-outlet-->`, renderedHtml);
     res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
   } catch (e) {
     next(e);
-  };
+  }
 };
-
