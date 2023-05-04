49c75c6ba1f2e17eb5dd48479241fbb52ff92acc
Artur Isaev <isaev.artur.1999@mail.ru>
2023-05-04 08:32:44 +0600

    fix: removed sort comparer from ratings entityadapter

--- packages/client/src/store/leaderboard/ratingEntityAdapter.ts
+++ packages/client/src/store/leaderboard/ratingEntityAdapter.ts
@@ -1,11 +1,8 @@
 import { createEntityAdapter } from '@reduxjs/toolkit';
 import type { PlayerRatingEntity } from '../api/leaderboard/leaderboard.api.types';
-import { DEFAULT_RATING_FIELD } from '../api/leaderboard/constants';
 
 const leaderboardRatingsAdapter = createEntityAdapter<PlayerRatingEntity>({
   selectId: ratingEntity => ratingEntity.userLogin,
-  sortComparer: (ratingOne, ratingTwo) =>
-    ratingTwo[DEFAULT_RATING_FIELD] - ratingOne[DEFAULT_RATING_FIELD],
 });
 
 const ratingsAdapterInitialState = leaderboardRatingsAdapter.getInitialState();
