280c508987300de11827097842946732e00569a4
Artur Isaev <isaev.artur.1999@mail.ru>
2023-05-05 06:28:56 +0600

    add logger injection to utils getNestedPropertByPath function

---
+++ packages/client/src/utils/objects-handle/get-nested-property/getNestedPropertyByPath.ts
@@ -0,0 +1,56 @@
+import { NestedPropertyType } from '../types/nestedProperty';
+import { ObjectKey } from '../types/objectType';
+import { isObject } from '../isObject';
+import type { LoggerContext, LoggerRelatedArgs } from './logger.types';
+
+function defaultLogger(this: LoggerContext) {
+  console.log(
+    `PATH GET '${this.pathArray}' EXISTING PART: ` +
+      `'${this.existingPath}'\n` +
+      `value: ${this.value}`
+  );
+}
+
+export function getNestedPropertyByPath<
+  InputObject,
+  ArrayLikePath extends ObjectKey[]
+>({
+  object,
+  pathArray,
+  isLogNeeded = true,
+  logger = defaultLogger,
+}: {
+  object: InputObject;
+  pathArray: ArrayLikePath;
+} & LoggerRelatedArgs):
+  | NestedPropertyType<InputObject, ArrayLikePath>
+  | undefined {
+  if (!isObject(object) || !pathArray.length) {
+    return undefined;
+  }
+
+  let value: any = object;
+  let existingPathIndex: number | null = null;
+  pathArray.every((currentPath, index) => {
+    const isPathExisting = isObject(value) && Object.hasOwn(value, currentPath);
+
+    if (isPathExisting) {
+      value = value[currentPath];
+      existingPathIndex = index;
+    }
+
+    return isPathExisting;
+  });
+
+  const existingPath =
+    existingPathIndex !== null ? pathArray.slice(0, existingPathIndex + 1) : [];
+  if (isLogNeeded) {
+    logger.call({
+      pathArray,
+      existingPath,
+      value,
+    });
+  }
+
+  return value;
+}
