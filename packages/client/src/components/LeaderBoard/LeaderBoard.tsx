cfdc9b2b38e2901a05832a32ce1e23ee4aea2eac
Artur Isaev <isaev.artur.1999@mail.ru>
2023-05-05 14:56:00 +0600

    remove rating post form demo functionality

--- packages/client/src/components/LeaderBoard/LeaderBoard.tsx
+++ packages/client/src/components/LeaderBoard/LeaderBoard.tsx
@@ -1,20 +1,13 @@
-import { FC, memo, useCallback, useState } from 'react';
+import { FC, memo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import hocAuth from '@/hoc/hocAuth';
 import RatingTable from './RatingTable/RatingTable';
 import SortMenu from './SortingMenu/SortingMenu';
 import './LeaderBoard.css';
-import AddCardForm from './PostRatingForm/PostRatingForm';
 import ShowMoreRatingsButton from './ShowMoreRatingsButton/ShowMoreRatingsButton';
 
 const LeaderBoard: FC = () => {
-  const [isAddFormActive, setIsAddFormActive] = useState<boolean>(false);
-
   const navigate = useNavigate();
-
-  const toggleAddCardForm = useCallback(() => {
-    setIsAddFormActive(isActive => !isActive);
-  }, []);
 
   return (
     <main className="leaderboard">
@@ -23,19 +16,8 @@ const LeaderBoard: FC = () => {
         <h1 className="leaderboard__title">Рейтинг игроков</h1>
       </div>
       <div className="leaderboard__container">
-        <section className="leaderboard__functional-section">
-          <SortMenu />
-          <button
-            className="leaderboard__post-rating-button leaderboard__functional-button"
-            onClick={toggleAddCardForm}>
-            Опубликовать рейтинг
-          </button>
-        </section>
-        <section className="leaderboard__rating-section">
-          {isAddFormActive && <AddCardForm />}
-          <RatingTable />
-        </section>
-
+        <SortMenu />
+        <RatingTable />
         <ShowMoreRatingsButton />
       </div>
     </main>
