ede585471670fa51cdcf6c8bf7ad3d42b99df1b9
quantum <partnerprods@yandex.ru>
2023-05-20 14:02:12 +0700

    feat: добавить манипуляции с пользователем

---
+++ packages/server/controllers/images.ts
@@ -0,0 +1,6 @@
+/* import type { NextFunction, Request, Response } from 'express';
+
+export const uploadImage = async () => {
+
+}
+ */
--- packages/server/routes/users.ts
+++ packages/server/routes/users.ts
@@ -1,7 +1,19 @@
 import { Router } from 'express';
-import { logoutUser } from '../controllers/users';
+import { changePassword, getCurrentUser, logoutUser, updateProfile } from '../controllers/users';
+import { 
+  changePasswordValidation, updateProfileValidation,
+ } from '../utils/validation/requestValidation';
 
 const router = Router();
+
+// Получить данные текущего пользователя
+router.get('/', getCurrentUser);
+
+// Обновить профиль
+router.patch('/profile', updateProfileValidation, updateProfile);
+
+// Изменить пароль
+router.patch('/password', changePasswordValidation, changePassword);
 
 // Выход пользователя из системы
 router.post('/logout', logoutUser);
