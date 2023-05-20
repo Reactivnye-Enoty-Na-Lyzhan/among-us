746be4f9e92787bb09eeb12c72d798158efcf5ad
Konstantin <rudnevaketi@gmail.com>
2023-05-20 18:52:33 +0300

    added try/catch

--- packages/server/controllers/message/messages.ts
+++ packages/server/controllers/message/messages.ts
@@ -1,18 +1,28 @@
-import type { Response } from 'express';
+import type { NextFunction, Response } from 'express';
 import type { RequestGetAllMessageByIdPost, RequestPostMessage } from '../../utils/type';
 import { MessageModel } from '../../models/forum/message';
+import { NotExistError } from '../../utils/errors/commonErrors/NotExistError';
+import { ErrorMessages } from '../../utils/errors/errorMessages';
 
-export const postMessage = async (req: RequestPostMessage, res: Response) => {
-  const data = await MessageModel.create({ ...req.body });
-  res.status(201).send(data.dataValues);
+export const postMessage = async (req: RequestPostMessage, res: Response, next: NextFunction) => {
+  try {
+    const data = await MessageModel.create({ ...req.body });
+    res.status(201).send(data.dataValues);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const getMessages = async (req: RequestGetAllMessageByIdPost, res: Response) => {
-  const { postId } = req.query;
-  const data = await MessageModel.findAll({ where: { postId: Number(postId) }, raw: true });
-  if (data && data.length) {
-    res.status(200).send(data);
-  } else {
-    res.status(404).json({ message: 'Not Found' });
+export const getMessages = async (req: RequestGetAllMessageByIdPost, res: Response, next: NextFunction) => {
+  try {
+    const { postId } = req.query;
+    const data = await MessageModel.findAll({ where: { postId: Number(postId) }, raw: true });
+    if (data && data.length) {
+      res.status(200).send(data);
+    } else {
+      throw new NotExistError(ErrorMessages.notFound);
+    }
+  } catch (err) {
+    next(err);
   }
 };
--- packages/server/controllers/post/posts.ts
+++ packages/server/controllers/post/posts.ts
@@ -1,23 +1,37 @@
-import type { Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 import { PostModel } from '../../models/forum/post';
+import { NotExistError } from '../../utils/errors/commonErrors/NotExistError';
 import type { RequestPostPost, RequestGetPostById } from '../../utils/type';
+import { ErrorMessages } from '../../utils/errors/errorMessages';
 
-export const postPost = async (req: RequestPostPost, res: Response) => {
-  const data = await PostModel.create({ ...req.body });
-  res.status(201).send(data.dataValues);
+export const postPost = async (req: RequestPostPost, res: Response, next: NextFunction) => {
+  try {
+    const data = await PostModel.create({ ...req.body });
+    res.status(201).send(data.dataValues);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const getPosts = async (_req: Request, res: Response) => {
-  const data = await PostModel.findAll();
-  res.status(200).send(data);
-};
-
-export const getPostById = async (req: RequestGetPostById, res: Response) => {
-  const { postId } = req.params;
-  const data = await PostModel.findOne({ where: { id: Number(postId) } });
-  if (data) {
+export const getPosts = async (_req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = await PostModel.findAll();
     res.status(200).send(data);
-  } else {
-    res.status(404).json({ message: 'Упс. Ничего не найдено.' });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getPostById = async (req: RequestGetPostById, res: Response, next: NextFunction) => {
+  try {
+    const { postId } = req.params;
+    const data = await PostModel.findOne({ where: { id: Number(postId) } });
+    if (data) {
+      res.status(200).send(data);
+    } else {
+      throw new NotExistError(ErrorMessages.notFound);
+    }
+  } catch (err) {
+    next(err);
   }
 };
