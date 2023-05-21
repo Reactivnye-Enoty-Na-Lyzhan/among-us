dcf04736116b2d95fd262da09aed068ffe416de6
Konstantin <rudnevaketi@gmail.com>
2023-05-21 09:18:49 +0300

    add routes

--- packages/server/controllers/message/messages.ts
+++ packages/server/controllers/message/messages.ts
@@ -1,27 +1,50 @@
 import type { NextFunction, Response } from 'express';
-import type { RequestGetAllMessageByIdPost, RequestPostMessage } from '../../utils/type';
-import { MessageModel } from '../../models/forum/message';
+import type { IRequestGetAllMessageByIdPost, IRequestPostMessage, IRequestDeleteMessage, IRequestReplyToMessage } from '../../utils/type';
+import { Message } from '../../models/forum/message';
 import { NotExistError } from '../../utils/errors/commonErrors/NotExistError';
 import { ErrorMessages } from '../../utils/errors/errorMessages';
 
-export const postMessage = async (req: RequestPostMessage, res: Response, next: NextFunction) => {
+export const postMessage = async (req: IRequestPostMessage, res: Response, next: NextFunction) => {
   try {
-    const data = await MessageModel.create({ ...req.body });
+    const data = await Message.create({ ...req.body });
     res.status(201).send(data.dataValues);
   } catch (err) {
     next(err);
   }
 };
 
-export const getMessages = async (req: RequestGetAllMessageByIdPost, res: Response, next: NextFunction) => {
+export const getMessages = async (req: IRequestGetAllMessageByIdPost, res: Response, next: NextFunction) => {
   try {
     const { postId } = req.query;
-    const data = await MessageModel.findAll({ where: { postId: Number(postId) }, raw: true });
+    const data = await Message.findAll({ where: { postId: Number(postId) }, raw: true });
     if (data && data.length) {
       res.status(200).send(data);
     } else {
       throw new NotExistError(ErrorMessages.notFound);
     }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteMessage = async (req: IRequestDeleteMessage, res: Response, next: NextFunction) => {
+  try {
+    const { messageId } = req.params;
+    const deleteMessage = await Message.destroy({ where: { id: messageId } });
+    if (deleteMessage === 0) {
+      throw new NotExistError(ErrorMessages.notFound);
+    }
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+export const replyToMessage = async (req: IRequestReplyToMessage, res: Response, next: NextFunction) => {
+  try {
+    const data = await Message.create({ ...req.body});
+    res.status(201).send(data.dataValues);
   } catch (err) {
     next(err);
   }
--- packages/server/controllers/post/posts.ts
+++ packages/server/controllers/post/posts.ts
@@ -1,12 +1,12 @@
 import type { NextFunction, Request, Response } from 'express';
-import { PostModel } from '../../models/forum/post';
+import { Post } from '../../models/forum/post';
 import { NotExistError } from '../../utils/errors/commonErrors/NotExistError';
-import type { RequestPostPost, RequestGetPostById } from '../../utils/type';
+import type { IRequestPostPost, IRequestGetPostById, IRequestDeletePost } from '../../utils/type';
 import { ErrorMessages } from '../../utils/errors/errorMessages';
 
-export const postPost = async (req: RequestPostPost, res: Response, next: NextFunction) => {
+export const postPost = async (req: IRequestPostPost, res: Response, next: NextFunction) => {
   try {
-    const data = await PostModel.create({ ...req.body });
+    const data = await Post.create({ ...req.body });
     res.status(201).send(data.dataValues);
   } catch (err) {
     next(err);
@@ -15,19 +15,34 @@ export const postPost = async (req: RequestPostPost, res: Response, next: NextFu
 
 export const getPosts = async (_req: Request, res: Response, next: NextFunction) => {
   try {
-    const data = await PostModel.findAll();
+    const data = await Post.findAll();
     res.status(200).send(data);
   } catch (err) {
     next(err);
   }
 };
 
-export const getPostById = async (req: RequestGetPostById, res: Response, next: NextFunction) => {
+export const getPostById = async (req: IRequestGetPostById, res: Response, next: NextFunction) => {
   try {
     const { postId } = req.params;
-    const data = await PostModel.findOne({ where: { id: Number(postId) } });
+    const data = await Post.findOne({ where: { id: Number(postId) } });
     if (data) {
       res.status(200).send(data);
+    } else {
+      throw new NotExistError(ErrorMessages.notFound);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+export const deletePost = async (req: IRequestDeletePost, res: Response, next: NextFunction) => {
+  try {
+    const { postId } = req.params;
+    const data = await Post.destroy({ where: { id: Number(postId) } });
+    if (data) {
+      res.status(204).end();
     } else {
       throw new NotExistError(ErrorMessages.notFound);
     }
