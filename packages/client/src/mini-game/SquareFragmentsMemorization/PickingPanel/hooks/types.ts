5420d998b007a0f4626dc393b88b46415bfdd0e3
Artur Isaev <isaev.artur.1999@mail.ru>
2023-05-24 06:14:00 +0600

    fix typization error

--- packages/client/src/mini-game/SquareFragmentsMemorization/PickingPanel/hooks/types.ts
+++ packages/client/src/mini-game/SquareFragmentsMemorization/PickingPanel/hooks/types.ts
@@ -1,5 +1,8 @@
 import type { PickingPanelContext } from '../types';
 
-export type WithPickingPanelContextArgs<Args = Record<string, never>> = {
+export type WithPickingPanelContextArgs<
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  Args extends Record<string, unknown> = {}
+> = {
   pickingPanelContext: PickingPanelContext;
 } & Args;
