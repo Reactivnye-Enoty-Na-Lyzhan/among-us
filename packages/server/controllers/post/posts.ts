7ed8f0d3b54dbfeb6bd2640a9da564e87106f6b2
Konstantin <rudnevaketi@gmail.com>
2023-05-27 08:56:19 +0300

    added helper function

--- packages/server/controllers/post/posts.ts
+++ packages/server/controllers/post/posts.ts
@@ -1,4 +1,4 @@
-import type { NextFunction, Request, Response } from 'express';
+import type { Request, Response } from 'express';
 import { Post } from '../../models/forum/post';
 import { NotExistError } from '../../utils/errors/commonErrors/NotExistError';
 import type {
@@ -7,67 +7,36 @@ import type {
   IRequestDeletePost,
 } from '../../types/forum/types';
 import { ErrorMessages } from '../../utils/errors/errorMessages';
+import { withErrorHandler } from '../../utils/errors/errorHandler';
 
-export const postPost = async (
-  req: IRequestPostPost,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { text, date, pinned } = req.body;
-    const authorId = req.user?.id;
-    const data = await Post.create({ text, authorId, date, pinned });
-    res.send(data.dataValues);
-  } catch (err) {
-    next(err);
-  }
-};
+export const postPost = withErrorHandler(async (req: IRequestPostPost, res: Response) => {
+  const { text, date, pinned } = req.body;
+  const authorId = req.user?.id;
+  const data = await Post.create({ text, authorId, date, pinned });
+  res.send(data.dataValues);
+});
 
-export const getPosts = async (
-  _req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const data = await Post.findAll();
-    res.send(data);
-  } catch (err) {
-    next(err);
-  }
-};
+export const getPosts = withErrorHandler(async (_req: Request, res: Response) => {
+  const data = await Post.findAll();
+  res.send(data);
+});
 
-export const getPostById = async (
-  req: IRequestGetPostById,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { postId } = req.params;
-    const parsedPostId = Number(postId);
-    const data = await Post.findOne({ where: { id: parsedPostId } });
-    if (!data) {
-      throw new NotExistError(ErrorMessages.notFound);
-    }
-    res.send(data);
-  } catch (err) {
-    next(err);
+export const getPostById = withErrorHandler(async (req: IRequestGetPostById, res: Response) => {
+  const { postId } = req.params;
+  const parsedPostId = Number(postId);
+  const data = await Post.findOne({ where: { id: parsedPostId } });
+  if (!data) {
+    throw new NotExistError(ErrorMessages.notFound);
   }
-};
+  res.send(data);
+});
 
-export const deletePost = async (
-  req: IRequestDeletePost,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { postId } = req.params;
-    const parsedPostId = Number(postId);
-    const data = await Post.destroy({ where: { id: parsedPostId } });
-    if (!data) {
-      throw new NotExistError(ErrorMessages.notFound);
-    }
-    res.json({ postId: parsedPostId });
-  } catch (err) {
-    next(err);
+export const deletePost = withErrorHandler(async (req: IRequestDeletePost, res: Response) => {
+  const { postId } = req.params;
+  const parsedPostId = Number(postId);
+  const data = await Post.destroy({ where: { id: parsedPostId } });
+  if (!data) {
+    throw new NotExistError(ErrorMessages.notFound);
   }
-};
+  res.json({ postId: parsedPostId });
+});
