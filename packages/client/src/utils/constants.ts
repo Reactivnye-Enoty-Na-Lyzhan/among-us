207796449db99bb68372b4db0320c8031e7d3f00
quantum <partnerprods@yandex.ru>
2023-06-01 18:19:16 +0700

    small fix

--- packages/client/src/utils/constants.ts
+++ packages/client/src/utils/constants.ts
@@ -1,15 +1,6 @@
-import ConnectWires from '@/components/Minigames/ConnectWires/ConnectWires';
-
 export const API_BASE_URL = '/api/';
 export const API_TEAM_NAME = 'enoty';
 export const SIGNIN_URL = '/signin';
 export const SOCKET_BASE_URL = 'localhost:3001';
 export const OAUTH_API_PATH = 'oauth/';
 export const DEFAULT_RESOURCE_URL = 'https://storage.yandexcloud.net/amongus';
-
-export const MINIGAMES = [
-  {
-    id: 'connwires',
-    component: ConnectWires,
-  },
-];
---
+++ packages/client/src/utils/game/mingames.ts
@@ -0,0 +1,8 @@
+import ConnectWires from '@/components/Minigames/ConnectWires/ConnectWires';
+
+export const MINIGAMES = [
+  {
+    id: 'connwires',
+    component: ConnectWires,
+  },
+];
