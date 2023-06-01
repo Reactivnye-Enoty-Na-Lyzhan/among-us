617f164c3ead2d0172c5f0ead58470c0a4d68fe1
Artur Issayev <32362017+Issayev@users.noreply.github.com>
2023-06-01 17:30:59 +0600

    Мини Игра (#61)

---
+++ packages/client/src/mini-game/SquareFragmentsMemorization/GameContext/useMemorizationSequence.tsx
@@ -0,0 +1,52 @@
+import { useMemo, useRef } from "react";
+
+export function useMemorizationSequence() {
+  const memorizationSequenceRef = useRef<
+    number[]
+  >([]);
+
+  return useMemo(() => {
+    const expandMemorizationSequence = () => {
+      const memorizationSequence =
+        memorizationSequenceRef.current;
+
+      expandSequenceWithRandomFragment(
+        memorizationSequence
+      );
+    };
+
+    return {
+      memorizationSequenceRef,
+      expandMemorizationSequence,
+    };
+  }, []);
+}
+
+const vacantFragmentsSet = new Set([
+  ...Array(9).keys(),
+]);
+
+function getNextRandomFragment() {
+  const randomFragment = [...vacantFragmentsSet][
+    Math.floor(
+      Math.random() * vacantFragmentsSet.size
+    )
+  ];
+
+  vacantFragmentsSet.delete(randomFragment);
+
+  return randomFragment;
+}
+
+function expandSequenceWithRandomFragment(
+  memorizationSequence: number[]
+) {
+  const nextFragment = getNextRandomFragment();
+  memorizationSequence.push(nextFragment);
+
+  console.log(
+    `NEXT SEQUENCE: ${JSON.stringify(
+      memorizationSequence
+    )}`
+  );
+}
